7beeed992c1dc44639df5a5d4ba553115c6626e4
crisine <crisine@knou.ac.kr>
2024-02-23 07:28:58 +0900

    feat: MVC 패턴 기준으로 VC 구현

--- SeSAC4MVVM/ViewController.swift
+++ SeSAC4MVVM/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var validationLabel: UILabel!
     
@@ -21,6 +20,25 @@ class ViewController: UIViewController {
     
     @objc private func didNumberTextFieldEditingChanged(sender: UITextField) {
         
+        guard let text = numberTextField.text else {
+            validationLabel.text = "값을 입력해주세요"
+            return
+        }
+        
+        guard let number = Int(text) else {
+            validationLabel.text = "숫자를 입력해주세요"
+            return
+        }
+        
+        if number < 0 || number > 1000000 {
+            validationLabel.text = "0 ~ 1,000,000 사이의 숫자를 입력해주세요"
+            return
+        }
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        validationLabel.text = formatter.string(for: number)
     }
 }
 
